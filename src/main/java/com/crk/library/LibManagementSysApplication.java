a45826ab1a1774b410ea3ee8b2ede57205e7b431
Kaviyarasan C <crkaviyarasan1029@gmail.com>
2026-07-07 10:53:11 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/crk/library/LibManagementSysApplication.java
@@ -0,0 +1,13 @@
+package com.crk.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibManagementSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibManagementSysApplication.class, args);
+    }
+
+}
